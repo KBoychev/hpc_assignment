cd9ac1b8819d3b2874bdead74ef39149371383c3
kboychev <boichev7@gmail.com>
2017-03-02 17:15:59 +0000

    Fixed node forces and moments. Everything agrees with the analytical solution.

--- functions/set_F_e.cpp
+++ functions/set_F_e.cpp
@@ -4,10 +4,10 @@
 void set_Fe(int n_e, double *F_e, double l, double qy) {
 
 	F_e[0] = 0;
-	F_e[1] = qy / 2;
-	F_e[2] = (qy * l) / 12;
+	F_e[1] = qy*l / 2;
+	F_e[2] = (qy * l*l) / 12;
 	F_e[3] = 0;
-	F_e[4] = qy / 2;
-	F_e[5] = -(qy * l) / 12;
+	F_e[4] = qy*l / 2;
+	F_e[5] = -(qy * l*l) / 12;
 
 }
