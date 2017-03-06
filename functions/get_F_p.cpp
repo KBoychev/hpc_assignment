cd8c5834c6c7dc6d7de7c7284eaeece5844119b1
kboychev <boichev7@gmail.com>
2017-03-06 00:02:07 +0000

    Task 4 done and verified with analytical solution. Parallelism works. Need to clear and format code.

--- functions/get_F_p.cpp
+++
@@ -1,9 +0,0 @@
-#include "functions.h"
-
-
-void get_F_p(int p,int &r, double *F, int &r_p, double *F_p) {
-
-	for(int i=0;i<r_p;i++){
-			F_p[i]=F[(i+p*6)];
-	}
-}
--- main_mpi.cpp
+++ main_mpi.cpp
@@ -26,23 +26,27 @@ int main(int argc, char* argv[]) {
 
   	MPI_status = MPI_Comm_rank(MPI_COMM_WORLD,&MPI_P_ID);
   
-  	//std::cout<<"Process "<<MPI_P_ID+1<<" of "<<MPI_NP<<std::endl;
-
-  	int N_e_p;
+ 
+ 
   	int N_e;
 	double l;
 	double T;
 	int N_t;
   	double dt;
-  	double *M;
-  	double *K;
-  	double t;
-  	int r;
-  	int r_e;
-  	double qy;
-  	double Fy;
+  	int r_p=3;
+  	double *M_p=new double[r_p*r_p]();
+  	double *K_p=new double[r_p*r_p]();
+  	double *K_pp=new double[r_p*r_p]();
+  	double *K_pm=new double[r_p*r_p]();
+  	double *F_p=new double[r_p]();
+  	double *u_p;
+  	double *u_pp;
+  	double *u_pm;
+  	double *u_buff=new double[r_p]();
 
+  	double t;
   	
+
  	if(MPI_P_ID==0){
 		
 		double L = 10.0; 
@@ -52,12 +56,16 @@ int main(int argc, char* argv[]) {
 		double rho = 7850; 
 
 
-		N_e = 4; 
-		N_e_p=N_e/2;
+		N_e = 24; 	
 		l =L/N_e;
 		T = 1; 
 		N_t = 10000;
 		dt=T/(N_t-1);
+
+
+		u_p=new double[(N_e/2+2)*3]();
+		u_pp=new double[(N_e/2+2)*3]();
+		u_pm=new double[(N_e/2+2)*3]();
 
 		MPI_Send(&N_e,1,MPI_INT,1,0,MPI_COMM_WORLD);	
 		MPI_Send(&l,1,MPI_DOUBLE,1,0,MPI_COMM_WORLD);	
@@ -65,7 +73,7 @@ int main(int argc, char* argv[]) {
 		MPI_Send(&N_t,1,MPI_INT,1,0,MPI_COMM_WORLD);	
 		MPI_Send(&dt,1,MPI_DOUBLE,1,0,MPI_COMM_WORLD);	
 
-		r_e = 6; 
+		int r_e=6;
 
 		double *M_e = new double[r_e*r_e]();
 
@@ -73,25 +81,45 @@ int main(int argc, char* argv[]) {
 
 		set_M_e(r_e,r_e, M_e, rho, A, l);
 
+		disp(r_e,r_e,M_e,"M_e");
+
 		set_K_e(r_e, r_e,K_e, E, A, I, l);
 
-		r = 3 * (N_e_p + 1); 
+		disp(r_e,r_e,K_e,"K_e");
 
-		K=new double[r*r]();
-	
-		M= new double[r*r]();
-	
-		get_M(r, r, M, r_e, M_e, N_e_p);
+		
+		M_p[0*r_p+0]=2*M_e[0*r_e+0];
+		M_p[1*r_p+1]=2*M_e[1*r_e+1];
+		M_p[2*r_p+2]=2*M_e[2*r_e+2];
 
-		//disp(r,r,M,"M");
+		//Cool stuff happens here. We don't need the large stiffness and mass
+		//matrices! The displacements at each node are dependent on the stiffness matrix of the
+		//node and the stiffness matrix of the adjacent nodes, therefore we can use the element 
+		//matrix only and extract the node stiffnesses matrices from there :D no memory needed for large matrices!
 
-		MPI_Send(M,r*r,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
+		MPI_Send(M_p,r_p*r_p,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
 
-		get_K(r, r, K, r_e, K_e, N_e_p);
+		K_p[0*r_p+0]=K_e[0*r_e+0]+K_e[3*r_e+3];
+		K_p[1*r_p+1]=K_e[1*r_e+1]+K_e[4*r_e+4];
+		K_p[2*r_p+2]=K_e[2*r_e+2]+K_e[5*r_e+5];
 
-		MPI_Send(K,r*r,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
+		MPI_Send(K_p,r_p*r_p,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
 
-		//disp(r,r,K,"K");
+		K_pp[0*r_p+0]=K_e[0*r_e+3];
+		K_pp[1*r_p+1]=K_e[1*r_e+4];
+		K_pp[1*r_p+2]=K_e[1*r_e+5];
+		K_pp[2*r_p+1]=K_e[2*r_e+4];
+		K_pp[2*r_p+2]=K_e[2*r_e+5];
+
+		MPI_Send(K_pp,r_p*r_p,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
+
+		K_pm[0*r_p+0]=K_e[3*r_e+0];
+		K_pm[1*r_p+1]=K_e[4*r_e+1];
+		K_pm[1*r_p+2]=K_e[4*r_e+2];
+		K_pm[2*r_p+1]=K_e[5*r_e+1];
+		K_pm[2*r_p+2]=K_e[5*r_e+2];
+		
+		MPI_Send(K_pm,r_p*r_p,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
 
 		delete[] K_e;
 
@@ -105,26 +133,21 @@ int main(int argc, char* argv[]) {
 
 	if(MPI_P_ID!=0){
 
-
   		MPI_Recv(&N_e,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+  		u_p=new double[(N_e/2+2)*3]();
+		u_pp=new double[(N_e/2+2)*3]();
+		u_pm=new double[(N_e/2+2)*3]();
+
+
   		MPI_Recv(&l,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 		MPI_Recv(&T,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 		MPI_Recv(&N_t,1,MPI_INT,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 		MPI_Recv(&dt,1,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-
-		N_e_p=N_e/2;
-		r_e=6;
-		r=3 * (N_e_p + 1);
-  		
-
-  		K=new double[r*r]();
-  		M=new double[r*r]();
-
-
-		MPI_Recv(M,r*r,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-
-		MPI_Recv(K,r*r,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-
+		MPI_Recv(M_p,r_p*r_p,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		MPI_Recv(K_p,r_p*r_p,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		MPI_Recv(K_pp,r_p*r_p,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+		MPI_Recv(K_pm,r_p*r_p,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
 		
 	}
 
@@ -133,25 +156,12 @@ int main(int argc, char* argv[]) {
 	//--------------------------------------------------------
 	
 
-
 	if(MPI_P_ID==0){
-		//std::cout<<"Solving [M]d2{u}/dt2+[K]{u}={F}"<<std::endl;
-		//std::cout<<std::endl;
+		std::cout<<"Solving [M]d2{u}/dt2+[K]{u}={F} on "<<MPI_NP<<" processes."<<std::endl;
+		std::cout<<std::endl;
 	}
 
 	
-
-	
-	
-
-	double *F_e = new double[r_e];
-	double *F = new double[r];
-	//double *F_p = new double[r]();
-	double *u= new double[r*3](); 
-	double *tmp = new double[r]();
-	double *buff = new double[3]();
-
-
 
 	for(int n_t=1;n_t<=N_t;n_t++){
 
@@ -161,138 +171,113 @@ int main(int argc, char* argv[]) {
 
 		}
 
-		//Get F (needs optimisation)
-		//-------------------------------
+		F_p[0]=0;
+		F_p[2]=0;
 
-		qy=t*1000.0/T;
-		Fy=0;
-
-		set_F_e(r_e,F_e,l,qy);
-
-		get_F(r,F,r_e,F_e,N_e_p,Fy);
-
-		Fy=t*1000.0/T;
 
 		if(MPI_P_ID==0){
-			F[0]=0;
-			F[1]=0;
-			F[2]=0;
-			F[r-2]=F[r-2]+Fy/2.0;
-		}
 
-		if(MPI_P_ID==1){
-			F[1]=F[1]+Fy/2.0;
-			F[r-3]=0;
-			F[r-2]=0;
-			F[r-1]=0;
-		}
+			for(int k=1;k<=(N_e/2);k++){
 
-		
+				if(k==(N_e/2)){
+					F_p[1]=t*1000.0/T*l+t*1000.0/T;
+				}else{
+					F_p[1]=t*1000.0/T*l;
+				}
 
-		//Get u at next timestep
-		//-------------------------------
+				for(int i=0;i<3;i++){
 
-		for(int i=0;i<r;i++){
+					u_pp[k*3+i]=0;
 
-			tmp[i]=0;
+					for(int j=0;j<3;j++){
 
-			for(int j=0;j<r;j++){
+						u_pp[k*3+i]=u_pp[k*3+i]-K_pm[i*r_p+j]*u_p[(k-1)*3+j]-K_p[i*r_p+j]*u_p[k*3+j]-K_pp[i*r_p+j]*u_p[(k+1)*3+j];
+					}
 
-				tmp[i]=tmp[i]-(K[i*r+j]-2.0/(dt*dt)*M[i*r+j])*u[j*3+1]-1.0/(dt*dt)*M[i*r+j]*u[j*3+2];
+					u_pp[k*3+i]=u_pp[k*3+i]+F_p[i]+2.0/(dt*dt)*M_p[i*r_p+i]*u_p[k*3+i]-1.0/(dt*dt)*M_p[i*r_p+i]*u_pm[k*3+i];
+					u_pp[k*3+i]=dt*dt*1.0/M_p[i*r_p+i]*u_pp[k*3+i];
 
+				}
 			}
 
-			tmp[i]=tmp[i]+F[i];
+			//send to proceess 1
 
-			u[i*3]=tmp[i]*dt*dt*1.0/M[i*r+i];
-		}
-
-		
-
-		//Send overlapping u
-		//-------------------------------
-
-		//Send u[r-3] from process 0 to process 1
-		//--------------------------------------------------
-		if(MPI_P_ID==0){
-
-			buff[0]=u[(r-3)*3];
-			buff[1]=u[(r-2)*3];
-			buff[2]=u[(r-1)*3];
-
-			MPI_Send(buff,3,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
-
-			//std::cout<<"Process 0 sent data!"<<std::endl;
-
-			//disp(3,1,buff,"buff");
-		}
-
-		//Receive u[r-3] u[r-2] u[r-1] from process 0 and send u[2] to process 0
-		//--------------------------------------------------
-		if(MPI_P_ID==1){
-
-			MPI_Recv(buff,3,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-
-			//std::cout<<"Process 1 received data!"<<std::endl;
-
-			u[0*3]=u[0*3]+buff[0];
-			u[1*3]=u[0*3]+buff[1];
-			u[2*3]=-u[0*3];//+buff[2];
-
-			buff[0]=u[0*3];
-			buff[1]=u[1*3];
-			buff[2]=u[2*3];
+			u_buff[0]=u_pp[(N_e/2-1)*3+0];
+			u_buff[1]=u_pp[(N_e/2-1)*3+1];
+			u_buff[2]=u_pp[(N_e/2-1)*3+2];
 
 
-			MPI_Send(buff,3,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
-
-			//std::cout<<"Process 1 sent data!"<<std::endl;
-		}
-
-		//Receive u[2] from process 1
-		//--------------------------------------------------
-		if(MPI_P_ID==0){
-
-			MPI_Recv(buff,3,MPI_DOUBLE,1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
-
-			u[(r-3)*3]=buff[0];
-			u[(r-2)*3]=buff[1];
-			u[(r-1)*3]=buff[2];
-
-			//std::cout<<"Process 0 received data!"<<std::endl;
-
-		}
-
-		//Set boundary conditions 
-	    //-------------------------------
-
-		if(MPI_P_ID==0){
-			u[0*3]=0;
-			u[1*3]=0;
-			u[2*3]=0;
-			//u[(r-1)*3]=0;
+			MPI_Send(u_buff,3,MPI_DOUBLE,1,0,MPI_COMM_WORLD);
+			
 		}
 
 		if(MPI_P_ID==1){
-			//u[2*3]=0;
-			u[(r-1)*3]=0;
-			u[(r-2)*3]=0;
-			u[(r-3)*3]=0;
+
+			//receive from proceess 0
+
+			MPI_Recv(u_buff,3,MPI_DOUBLE,0,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+			u_pp[0]=u_buff[0];
+			u_pp[1]=u_buff[1];
+			u_pp[2]=u_buff[2];
+
+
+			for(int k=1;k<=(N_e/2);k++){
+
+				if(k==1){
+					F_p[1]=t*1000.0/T*l+t*1000.0/T;
+				}else{
+					F_p[1]=t*1000.0/T*l;
+				}
+
+				for(int i=0;i<3;i++){
+
+					u_pp[k*3+i]=0;
+
+					for(int j=0;j<3;j++){
+
+						u_pp[k*3+i]=u_pp[k*3+i]-K_pm[i*r_p+j]*u_p[(k-1)*3+j]-K_p[i*r_p+j]*u_p[k*3+j]-K_pp[i*r_p+j]*u_p[(k+1)*3+j];
+					}
+
+					u_pp[k*3+i]=u_pp[k*3+i]+F_p[i]+2.0/(dt*dt)*M_p[i*r_p+i]*u_p[k*3+i]-1.0/(dt*dt)*M_p[i*r_p+i]*u_pm[k*3+i];
+					u_pp[k*3+i]=dt*dt*1.0/M_p[i*r_p+i]*u_pp[k*3+i];
+
+				}
+			}
+
+			//send to process 0
+
+			u_buff[0]=u_pp[6];
+			u_buff[1]=u_pp[7];
+			u_buff[2]=u_pp[8];
+
+			MPI_Send(u_buff,3,MPI_DOUBLE,0,0,MPI_COMM_WORLD);
+
 		}
 
-		//Save u values 
-	    //-------------------------------
 
-		for(int i=0;i<r;i++){
-			u[i*3+2]=u[i*3+1];
-			u[i*3+1]=u[i*3];
-		}
+		if(MPI_P_ID==0){
+
+			//receive from process 1
+
+			MPI_Recv(u_buff,3,MPI_DOUBLE,1,0,MPI_COMM_WORLD,MPI_STATUS_IGNORE);
+
+			u_pp[(N_e/2+1)*3+0]=u_buff[0];
+			u_pp[(N_e/2+1)*3+1]=u_buff[1];
+			u_pp[(N_e/2+1)*3+2]=u_buff[2];
 
 		
+		}
+
+
+
+		for(int i=0;i<((N_e/2+2)*3);i++){
+			u_pm[i]=u_p[i];
+			u_p[i]=u_pp[i];
+		}
 
 		t=t+dt;
 	}
-
 
 	//Gather/reduce results
 	//-------------------------------
@@ -301,10 +286,10 @@ int main(int argc, char* argv[]) {
 
 		std::cout<<"Done!"<<std::endl;
 		std::cout<<std::endl;
-		disp(r,3,u,"u");
+
+		disp((N_e/2+2)*3,1,u_pp,"u_pp");
 
 	}
-
 
 	MPI_Finalize();
 
