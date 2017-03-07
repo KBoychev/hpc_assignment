cfde78c6c0f73c31eaba276356eb8cbea6e4ae7a
kboychev <boichev7@gmail.com>
2017-03-07 21:22:34 +0000

    Added command line parameter input.

--- main.cpp
+++ main.cpp
@@ -2,42 +2,152 @@
 
 #include <iostream>
 #include <iomanip>
-
+#include <string>
 
 #include "functions.h"
 
 
-int main() {
+int main(int argc, char *argv[]) {
 
 
-
+	int N_e = 0;
+	double L = 0;	
+	double A = 0;
+	double I = 0;
+	double E = 0;
+	double rho = 0;
+	double T = 0;
+	int N_t = 0;
+	int eq;
+	int sch;
+	
 	//// Beam and element geometry and material properties
-	//--------------------------------------------------------
+	//-----------------------------------------------------
+	if (argc>16){
 
-	int N_e = 26; // - (number of elements)
-	double L = 10.0; // m (beam length)
-	double l = L / N_e; //m (element length)
-
-	double A = 0.1*0.12; //m^2 (cross-sectional area)
-	double I = (0.1*0.12*0.12*0.12)/12; //m^4 (second moment of area)
-	double E = 210000000000; //2.1*pow(10,11); //Pa (Young's modulus)
-	double rho = 7850; //kg/m^3 (material density)
-	double T = 1; //s (end time)
-	int N_t = 10000; //- (number of time steps)
-	double dt=T/(N_t-1); //s (timestep)
-	double t=0; //s (time)
-	double qy=1000;
-	double Fy=1000;
+		for (int i=1; i<argc; i++){
 
 
-	std::cout<<std::setprecision(6);
+			if(std::strcmp(argv[i],"-L")==0){
+				try{
+					L=std::atof(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "L argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "L argument is out of range for a double!\n";
+					return 1;
+				}		
+			}
+			if(std::strcmp(argv[i],"-N_e")==0){
+				try{
+					N_e=std::stoi(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "N_e argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "N_e argument is out of range for a int!\n";
+					return 1;
+				}
+			}
+			if(std::strcmp(argv[i],"-A")==0){
+				try{
+					A=std::stod(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "A argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "A argument is out of range for a double!\n";
+					return 1;
+				}	A=std::stod(argv[i+1]);
+			}
+			if(std::strcmp(argv[i],"-I")==0){
+				try{
+					I=std::stod(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "I argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "I argument is out of range for a double!\n";
+					return 1;
+				}	
+			}
+			if(std::strcmp(argv[i],"-E")==0){
+				try{
+					E=std::stod(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "E argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "E argument is out of range for a double!\n";
+					return 1;
+				}	
+			}
+			if(std::strcmp(argv[i],"-T")==0){
+			 	try{
+					T=std::stod(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "T argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "T argument is out of range for a double!\n";
+					return 1;
+				}
+			}
+			if(std::strcmp(argv[i],"-N_t")==0){
+			 	try{
+					N_t=std::stoi(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "N_t argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "N_t argument is out of range for a int!\n";
+					return 1;
+				}
+			}
+			if(std::strcmp(argv[i],"-rho")==0){
+			 	try{
+					rho=std::stod(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "Rho argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "Rho argument is out of range for a double!\n";
+					return 1;
+				}
+			}
+			if(std::strcmp(argv[i],"-eq")==0){
+				try{
+					eq=std::stoi(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "Eq argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "Eq argument is out of range for a int!\n";
+					return 1;
+				}
+			}
 
-	std::cout<<"l:"<<l<<"m"<<std::endl;
-	std::cout<<"A:"<<A<<"m2"<<std::endl;
-	std::cout<<"I:"<<I<<"m4"<<std::endl;
-	std::cout<<"E:"<<E<<"Pa"<<std::endl;
-	std::cout<<"dt:"<<dt<<"s"<<std::endl;
+			if(std::strcmp(argv[i],"-sch")==0){
+				try{
+					sch=std::stoi(argv[i+1]);
+				}catch(const std::invalid_argument&){
+					std::cout << "Sch argument is invalid!\n";
+					return 1;
+				}catch(const std::out_of_range&){
+					std::cout << "Sch argument is out of range for a int!\n";
+					return 1;
+				}
+			}
+		}
 
+	}else{
+		std::cout << "Not enough arguments!\n";
+		return 1;
+	}
+
+	double l = L / N_e;
+	double dt = T / ( N_t - 1);
 
 	//// Element mass,stiffness, force matrices 
 	//	 and vectors
@@ -61,10 +171,7 @@ int main() {
 	double *K = new double[r*c]();
 	double *F = new double[r]();
 	double *u = new double[r]();
-	double *u_p = new double[r](); //u at previous time step
-	double *u_n = new double[r](); //u at next timestep
-
-
+	
 	//// Set element mass matrix [M_e]
 	//--------------------------------------------------------
 
@@ -79,12 +186,6 @@ int main() {
 
 	disp(r_e, c_e, K_e, "K_e");
 
-	//// Set element load vector {F_e}
-	//--------------------------------------------------------
-
-	set_F_e(r_e, F_e, l,qy);
-
-	disp(r_e,1,F_e,"F_e");
 
 	//// Get global mass matrix [M]
 	//--------------------------------------------------------
@@ -113,77 +214,31 @@ int main() {
 
 	disp(r, c, K, "K");
 
-	//// Get global load vector {F} and set boundary conditions
-	//--------------------------------------------------------
-
-	get_F(r, F, r_e, F_e, N_e,Fy);
-
-	F[(r-1)/2]=F[(r-1)/2]+Fy;
-
-	F[0]=0;
-	F[1]=0;
-	F[2]=0;
-
-	F[r-3]=0;
-	F[r-2]=0;
-	F[r-1]=0;
-
-	disp(r,1,F,"F");
-
-	//// Free memory from element matrices that are not going to be used again
-	//--------------------------------------------------------
-
-	delete[] M_e;
-	delete[] K_e;
 
 	//// Solve for static problem [K]{u}={F}. Solution
 	// 	 is done with conjugate gradient descent algorithm,
 	//   implemented with BLAS library routines.
 	//--------------------------------------------------------
 
-	
+	std::cout<<eq<<std::endl;
+	std::cout<<sch<<std::endl;
 
-	// std::cout<<std::endl;
- 	// 	std::cout<<"Solving [K]{u}={F}"<<std::endl;
-
-	// inv(K,r); //The inverse function ovverwrites the matrix [K] !
-
-	// disp(r,c,K,"K_inv");
-
-	// for(int i=0;i<r;i++){
-	// 	u[i]=0;
-	// 	for(int j=0;j<c;j++){
-	// 		u[i]=u[i]+K[i*r+j]*F[j];
-	// 	}
-	// }
-
-	// std::cout<<"Done!"<<std::endl;
-
- 	// std::cout<<std::endl;
-
-	// disp(r,1,u,"u");
-
-	//// Solve the dynamic problem [M]d2{u}/dt2+[K]{u}={F}. Solution
-	//	 is performed with explicit integration method.
-	//--------------------------------------------------------
-
-	std::cout<<"Solving [M]d2{u}/dt2+[K]{u}={F}"<<std::endl;
-
-	std::cout<<std::endl;
+	if(eq==0){
 
 
-	double *tmp = new double[r]();
 
-	
-	for(int n_t=1;n_t<=N_t;n_t++){
+		double qy=1000;
+		double Fy=1000;
 
-		std::cout<<"Iteration "<<n_t<<" t="<<t<<std::endl;
-
-		qy=t*1000.0/T;
-
-		Fy=t*1000.0/T;
+		//// Set element load vector {F_e}
+		//--------------------------------------------------------
 
 		set_F_e(r_e, F_e, l,qy);
+
+		disp(r_e,1,F_e,"F_e");
+
+		//// Get global load vector {F} and set boundary conditions
+		//--------------------------------------------------------
 
 		get_F(r, F, r_e, F_e, N_e,Fy);
 
@@ -197,190 +252,237 @@ int main() {
 		F[r-2]=0;
 		F[r-1]=0;
 
-		//Get u(n+1)
-		//-------------------------------
+		disp(r,1,F,"F");
+
+
+		std::cout<<std::endl;
+	 	std::cout<<"Solving [K]{u}={F}"<<std::endl;
+
+		inv(K,r); //The inverse function ovverwrites the matrix [K] !
+
+		disp(r,c,K,"K_inv");
+
 		for(int i=0;i<r;i++){
-
-			tmp[i]=0;
-
+			u[i]=0;
 			for(int j=0;j<c;j++){
-
-				tmp[i]=tmp[i]-(K[i*r+j]-2.0/(dt*dt)*M[i*r+j])*u[j]-1.0/(dt*dt)*M[i*r+j]*u_p[j];
-
+				u[i]=u[i]+K[i*r+j]*F[j];
 			}
-
-			tmp[i]=tmp[i]+F[i];
-
-			u_n[i]=tmp[i]*dt*dt*1.0/M[i*r+i];
 		}
 
-		//Set boundary conditions
-		//-------------------------------
+		std::cout<<"Done!"<<std::endl;
 
-		u_n[0]=0;
-		u_n[1]=0;
-		u_n[2]=0;
-		u_n[r-1]=0;
-		u_n[r-2]=0;
-		u_n[r-3]=0;
+	 	std::cout<<std::endl;
 
-		
-		for(int i=0;i<r;i++){
-			u_p[i]=u[i];
-			u[i]=u_n[i];
-		}
-		
-
-		t=t+dt;
-
+		disp(r,1,u,"u");
 
 	}
 
-
-	delete[] u_p;
-
-	std::cout<<std::endl;
-
-	std::cout<<"Done!"<<std::endl;
-
- 	std::cout<<std::endl;
-
-	disp(r,1,u,"u");
-	
-	
+	if(eq==1){
 
 	//// Solve the dynamic problem [M]d2{u}/dt2+[K]{u}={F}. Solution
-	//	 is performed with implicit integration method.
+	//	 is performed with explicit integration method.
 	//--------------------------------------------------------
 
-	// std::cout<<"Solving [M]d2{u}/dt2+[K]{u}={F}"<<std::endl;
-
-	// memset(u,0,r*sizeof(*u));
-
-	// double *u_tt = new double[r]();
-	// double *u_tt_n = new double[r]();
-	// double *u_t = new double[r]();
-	// double *u_t_n = new double[r]();
-	// double *K_eff = new double[r*c]();
+		std::cout<<"Solving [M]d2{u}/dt2+[K]{u}={F}"<<std::endl;
+		std::cout<<std::endl;
 
 
-	// for(int i=0;i<r;i++){
-	// 	for(int j=0;j<r;j++){
-	// 		K_eff[i*r+j]=4.0/(dt*dt)*M[i*r+j]+K[i*r+j];
-	// 	}
-	// }
-
-	// disp(r,c,K_eff,"K_eff");
-
-	// inv(K_eff,r);
-
-	// disp(r,c,K_eff,"K_eff");
+		double t=0;
+		double qy=0;
+		double Fy=0;
 
 
-	// std::cout<<std::endl;
+		double *u = new double[r]();
+		double *u_n = new double[r](); 
+		double *tmp = new double[r]();
+
+		if(sch==0){
+
+			
+			double *u_p = new double[r](); 
+			
+
+
+			for(int n_t=0;n_t<=N_t;n_t++){
+
+				std::cout<<"Iteration "<<n_t<<" t="<<t<<std::endl;
+
+				qy=t*1000.0/T;
+
+				Fy=t*1000.0/T;
+
+				set_F_e(r_e, F_e, l,qy);
+
+				get_F(r, F, r_e, F_e, N_e,Fy);
+
+				F[(r-1)/2]=F[(r-1)/2]+Fy;
+
+				F[0]=0;
+				F[1]=0;
+				F[2]=0;
+
+				F[r-3]=0;
+				F[r-2]=0;
+				F[r-1]=0;
+
+				//Get u(n+1)
+				//-------------------------------
+				for(int i=0;i<r;i++){
+
+					tmp[i]=0;
+
+					for(int j=0;j<c;j++){
+
+						tmp[i]=tmp[i]-(K[i*r+j]-2.0/(dt*dt)*M[i*r+j])*u[j]-1.0/(dt*dt)*M[i*r+j]*u_p[j];
+
+					}
+
+					tmp[i]=tmp[i]+F[i];
+
+					u_n[i]=tmp[i]*dt*dt*1.0/M[i*r+i];
+				}
+
+				//Set boundary conditions
+				//-------------------------------
+
+				u_n[0]=0;
+				u_n[1]=0;
+				u_n[2]=0;
+				u_n[r-1]=0;
+				u_n[r-2]=0;
+				u_n[r-3]=0;
+
+				
+				for(int i=0;i<r;i++){
+					u_p[i]=u[i];
+					u[i]=u_n[i];
+				}
+
+				t=t+dt;
+
+			}
+
+		}
+
+
+		if(sch==1){
+
+			double *u_tt = new double[r]();
+			double *u_tt_n = new double[r]();
+			double *u_t = new double[r]();
+			double *u_t_n = new double[r]();
+			double *K_eff = new double[r*c]();
+
+			for(int i=0;i<r;i++){
+				for(int j=0;j<r;j++){
+					K_eff[i*r+j]=4.0/(dt*dt)*M[i*r+j]+K[i*r+j];
+				}
+			}
+
+			disp(r,c,K_eff,"K_eff");
+
+			inv(K_eff,r);
+
+			for(int n_t=0;n_t<=N_t;n_t++){
+
+				std::cout<<"Iteration "<<n_t<<" t="<<t<<std::endl;
+
+				qy=t*1000.0/T;
+
+				Fy=t*1000.0/T;
+
+				set_F_e(r_e, F_e, l,qy);
+
+				get_F(r, F, r_e, F_e, N_e,Fy);
+
+				F[(r-1)/2]=F[(r-1)/2]+Fy;
+
+				F[0]=0;
+				F[1]=0;
+				F[2]=0;
+
+				F[r-3]=0;
+				F[r-2]=0;
+				F[r-1]=0;
+
+				//Get u_n
+				//-------------------------------
+				for(int i=0;i<r;i++){
+
+					tmp[i]=0;
+
+					for(int j=0;j<c;j++){
+
+						tmp[i]=tmp[i]+M[i*r+j]*(4.0/(dt*dt)*u[j]+4.0/dt*u_t[j]+u_tt[j]);
+
+					}
+
+					tmp[i]=tmp[i]+F[i];
+				}
+
+
+				for(int i=0;i<r;i++){
+
+					u_n[i]=0;
+
+					for(int j=0;j<c;j++){
+						u_n[i]=u_n[i]+K_eff[i*r+j]*tmp[j];
+
+					}
+
+					u_tt_n[i]=4.0/(dt*dt)*(u_n[i]-u[i])-4.0/dt*u_t[i]-u_tt[i];
+
+					u_t_n[i]=u[i]+dt*(1-0.5)*u_tt[i]+dt*0.5*u_tt_n[i];
+				}
+
+
+				//Set boundary conditions
+				//-------------------------------
+
+				u_n[0]=0;
+				u_n[1]=0;
+				u_n[2]=0;
+				u_n[r-1]=0;
+				u_n[r-2]=0;
+				u_n[r-3]=0;
+
+				u_t_n[0]=0;
+				u_t_n[1]=0;
+				u_t_n[2]=0;
+				u_t_n[r-1]=0;
+				u_t_n[r-2]=0;
+				u_t_n[r-3]=0;
+
+				u_tt_n[0]=0;
+				u_tt_n[1]=0;
+				u_tt_n[2]=0;
+				u_tt_n[r-1]=0;
+				u_tt_n[r-2]=0;
+				u_tt_n[r-3]=0;
+
+				
+				for(int i=0;i<r;i++){
+					u[i]=u_n[i];
+					u_t[i]=u_t_n[i];
+					u_tt[i]=u_tt_n[i];
+				}
+
+				t=t+dt;
+
+			}
+
+		}
 	
-	// for(int n_t=1;n_t<=2;n_t++){
 
-	// 	//std::cout<<"Iteration "<<n_t<<" t="<<t<<std::endl;
+		std::cout<<std::endl;
 
-	// 	qy=(t+dt)*1000.0/T;
+		std::cout<<"Done!"<<std::endl;
 
-	// 	Fy=(t+dt)*1000.0/T;
+	 	std::cout<<std::endl;
 
-	// 	set_Fe(r_e, F_e, l,qy);
+		disp(r,1,u,"u");
 
-	// 	get_F(r, F, r_e, F_e, N_e,Fy);
-
-
-	// 	//Get u_n
-	// 	//-------------------------------
-	// 	for(int i=0;i<r;i++){
-
-	// 		tmp[i]=0;
-
-	// 		for(int j=0;j<c;j++){
-
-	// 			tmp[i]=tmp[i]+M[i*r+j]*(4.0/(dt*dt)*u[j]+4.0/dt*u_t[j]+u_tt[j]);
-
-	// 		}
-
-	// 		tmp[i]=tmp[i]+F[i];
-	// 	}
-
-
-	// 	for(int i=0;i<r;i++){
-
-	// 		u_n[i]=0;
-
-	// 		for(int j=0;j<c;j++){
-	// 			u_n[i]=u_n[i]+K_eff[i*r+j]*tmp[j];
-
-	// 		}
-
-	// 		u_tt_n[i]=4.0/(dt*dt)*(u_n[i]-u[i])-4.0/dt*u_t[i]-u_tt[i];
-
-	// 		u_t_n[i]=u[i]+dt*(1-0.5)*u_tt[i]+dt*0.5*u_tt_n[i];
-	// 	}
-
-	// 	disp(r,1,F,"F");
-	// 	disp(r,1,tmp,"tmp");
-	// 	disp(r,1,u_n,"u");
-	// 	disp(r,1,u_tt_n,"u_tt");
-	// 	disp(r,1,u_t_n,"u_t");
-
-	// 	//Set boundary conditions
-	// 	//-------------------------------
-
-	// 	u_n[0]=0;
-	// 	u_n[1]=0;
-	// 	u_n[2]=0;
-	// 	u_n[r-1]=0;
-	// 	u_n[r-2]=0;
-	// 	u_n[r-3]=0;
-
-	// 	u_t_n[0]=0;
-	// 	u_t_n[1]=0;
-	// 	u_t_n[2]=0;
-	// 	u_t_n[r-1]=0;
-	// 	u_t_n[r-2]=0;
-	// 	u_t_n[r-3]=0;
-
-	// 	u_tt_n[0]=0;
-	// 	u_tt_n[1]=0;
-	// 	u_tt_n[2]=0;
-	// 	u_tt_n[r-1]=0;
-	// 	u_tt_n[r-2]=0;
-	// 	u_tt_n[r-3]=0;
-
-		
-	// 	for(int i=0;i<r;i++){
-	// 		u[i]=u_n[i];
-	// 		u_t[i]=u_t_n[i];
-	// 		u_tt[i]=u_tt_n[i];
-	// 	}
-
-	// 	disp(r,1,u,"u");
-		
-
-	// 	t=t+dt;
-
-	// }
-
-
-	// delete[] u_tt;
-	// delete[] u_tt_n;
-	// delete[] u_t;
-	// delete[] u_t_n;
-	// delete[] tmp;
-
-	// std::cout<<std::endl;
-
-	// std::cout<<"Done!"<<std::endl;
-
- // 	std::cout<<std::endl;
-
-	// disp(r,1,u,"u");
-
+	}
 
 	return 0;
 }
--- makefile
+++ makefile
@@ -1,13 +1,13 @@
-C=g++
+C=g++ -std=c++11
 CFLAGS=-c -I./
 FNCSD=./functions/
 
-default: task1 clean run
+default: task clean run1 run2 run3
 
-all: task1 clean run
+all: task clean run1 run2 run3
 
-task1: main.o set_M_e.o set_K_e.o set_F_e.o get_M.o get_K.o get_F.o inv.o disp.o	
-	$(C) main.o $(FNCSD)set_M_e.o $(FNCSD)set_K_e.o $(FNCSD)set_F_e.o $(FNCSD)get_M.o $(FNCSD)get_K.o $(FNCSD)get_F.o $(FNCSD)inv.o $(FNCSD)disp.o -llapack -lblas -o task1.out
+task: main.o set_M_e.o set_K_e.o set_F_e.o get_M.o get_K.o get_F.o inv.o disp.o	
+	$(C) main.o $(FNCSD)set_M_e.o $(FNCSD)set_K_e.o $(FNCSD)set_F_e.o $(FNCSD)get_M.o $(FNCSD)get_K.o $(FNCSD)get_F.o $(FNCSD)inv.o $(FNCSD)disp.o -llapack -lblas -o task.out
 
 main.o: 	
 	$(C) $(CFLAGS) main.cpp -o main.o
@@ -19,7 +19,6 @@ set_K_e.o:
 	$(C) $(CFLAGS) $(FNCSD)set_K_e.cpp -o $(FNCSD)set_K_e.o
 
 set_F_e.o:
-
 	$(C) $(CFLAGS) $(FNCSD)set_F_e.cpp -o $(FNCSD)set_F_e.o
 
 get_M.o:
@@ -45,6 +44,14 @@ clean:
 	rm $(FNCSD)get_M.o
 	rm $(FNCSD)get_K.o
 	rm $(FNCSD)get_F.o
+	rm $(FNCSD)inv.o
+	rm $(FNCSD)disp.o
 
-run:
-	./task1.out
+run1:
+	./task.out -L 10.0 -N_e 4 -A 0.012 -I 0.0000144 -E 210000000000.0 -T 1.0 -N_t 10000 -rho 7850.0 -eq 0
+
+run2:
+	./task.out -L 10.0 -N_e 4 -A 0.012 -I 0.0000144 -E 210000000000.0 -T 1.0 -N_t 10000 -rho 7850.0 -eq 1 -sch 0
+	
+run3:
+	./task.out -L 10.0 -N_e 4 -A 0.012 -I 0.0000144 -E 210000000000.0 -T 1.0 -N_t 10000 -rho 7850.0 -eq 1 -sch 1
